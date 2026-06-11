cb9c3fbc25a8b59d34d7c28fd03b2f89a3939cc4
sean-sqds <sean@sqds.io>
2026-06-11 09:45:07 -0400

    fix(integers): check integers for authority

--- src/lib/menu.ts
+++ src/lib/menu.ts
@@ -227,14 +227,14 @@ class Menu{
 
         if (assemble.indexOf("Assemble") == 0) {
             const {authority} = await createTransactionInq();
-            const authorityPDA = await this.api.getAuthority(ms.publicKey, parseInt(authority, 10));
+            const authorityPDA = await this.api.getAuthority(ms.publicKey, authority);
 
             const status = new Spinner("Creating transaction...");
             console.log("This will create a new transaction draft for authority " + chalk.blue(authorityPDA.toBase58()));
             const {yes} = await basicConfirm("Continue?", false);
             if (!yes) return () => this.multisig(ms);
             status.start();
-            const tx = await this.api.createTransaction(ms.publicKey, parseInt(authority, 10));
+            const tx = await this.api.createTransaction(ms.publicKey, authority);
             status.stop();
             console.log("Transaction created!");
             console.log("Transaction key: " + chalk.blue(tx.publicKey.toBase58()));
@@ -244,7 +244,7 @@ class Menu{
         }
         if (assemble.indexOf("Enter") == 0) {
             const {authority} = await createTransactionInq();
-            const authorityPDA = await this.api.getAuthority(ms.publicKey, parseInt(authority, 10));
+            const authorityPDA = await this.api.getAuthority(ms.publicKey, authority);
 
             const {rawIx} = await addTransactionInq();
             if (rawIx.length <= 1) return () => this.multisig(ms);
@@ -261,7 +261,7 @@ class Menu{
                 const {yes} = await basicConfirm(`Create a transaction with ${ixes.length} instructions?`, false);
                 if (!yes) return () => this.multisig(ms);
                 status.start();
-                const tx = await this.api.createTransaction(ms.publicKey, parseInt(authority, 10));
+                const tx = await this.api.createTransaction(ms.publicKey, authority);
                 status.stop();
                 console.log(`Transaction ${tx.publicKey.toBase58()} created!`);
                 for (let i = 0; i < ixes.length; i++) {
